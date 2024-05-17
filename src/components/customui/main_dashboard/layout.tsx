b9166acf537bf0a9445a4f85dad9146f9bdcbefa
stivex001 <stephenadeyemo@gmail.com>
2024-05-17 19:15:12 +0100

    feat:implemented dashbaord ui

--- src/components/customui/main_dashboard/layout.tsx
+++ src/components/customui/main_dashboard/layout.tsx
@@ -2,37 +2,36 @@ import React from "react";
 import { SideNav } from "./sidenav";
 import { TopNav } from "../../global/topnav";
 
-export const DashBoardLayout=({
-    children
-}:{
-    children: React.ReactNode;
-  })=>{
-    return(
-        <div className="bg-[#f9fafb] h-screen px-2 relative">
-            <div className="mb-4">
-                <TopNav/>
+export const DashBoardLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="bg-[#f9fafb] h-screen px-2 relative">
+      <div className="mb-4">
+        <TopNav />
+      </div>
+      <div className="mobileNav">
+        <SideNav />
+      </div>
+      <div className="w-full h-full">
+        <div
+          className="flex bg-cl"
+          style={{
+            height: "100%",
+          }}
+        >
+          <div className="h-[100%]">
+            <SideNav style="relative desktopNav w-60 overflow-hidden dark:bg-zinc-800 bg-white shadow me-2" />
+          </div>
+          <div className="w-full h-full lg:pt-8 xl:pt-8 md:pt-8 sm:pt-2 xs:pt-2 overflow-auto bg-white rounded-md">
+            <div className="px-4  overflow-auto w-full h-full">
+              {children}
             </div>
-            <div className="mobileNav">
-                <SideNav/>
-            </div>
-            <div className="w-full h-full">
-                <div 
-                    className="flex bg-cl"
-                    style={{
-                        height:"100%"
-                    }}
-                >
-                    <div className="h-[100%]">
-                        <SideNav 
-                            style="relative desktopNav w-60 overflow-hidden dark:bg-zinc-800 bg-white shadow me-2"/>
-                    </div>
-                    <div className="w-full h-full lg:pt-8 xl:pt-8 md:pt-8 sm:pt-2 xs:pt-2 overflow-auto bg-white rounded-md">
-                        <div className="px-4 h-full overflow-auto w-full h-full">
-                            {children}
-                        </div>
-                    </div>
-                </div>
-            </div>
-       </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/components/global/collapsibleComponent.tsx
+++ src/components/global/collapsibleComponent.tsx
@@ -1,60 +1,48 @@
-import * as React from "react"
-import { ChevronsUpDown} from "lucide-react"
- 
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+import { ChevronsUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "@/components/ui/collapsible"
-import { Text } from "./text"
- 
+} from "@/components/ui/collapsible";
+import { Text } from "./text";
+
 export function CollapsibleComponent({
-    title,
-    subTitle,
-    children
-}:{
-    title:string|React.ReactNode,
-    subTitle?:string,
-    children:React.ReactNode
+  title,
+  subTitle,
+  children,
+}: {
+  title: string | React.ReactNode;
+  subTitle?: string;
+  children: React.ReactNode;
 }) {
-  const [isOpen, setIsOpen] = React.useState(true)
- 
+  const [isOpen, setIsOpen] = React.useState(true);
+
   return (
     <div className="h-fit-content overflow-auto">
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="w-full space-y-2 h-fit-content overflow-auto"
-    >
-      <div className="flex items-center justify-between space-x-4 px-4">
-        <div
-           className="text-md font-semibold w-full"
-        >
-          {title}
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-full space-y-2 h-fit-content overflow-auto"
+      >
+        <div className="flex items-center justify-between space-x-4 px-4">
+          <div className="text-md font-semibold w-full">{title}</div>
+          <CollapsibleTrigger asChild>
+            <Button variant="ghost" size="sm" className="w-9 p-0">
+              <ChevronsUpDown className="h-4 w-4" />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </CollapsibleTrigger>
         </div>
-        <CollapsibleTrigger asChild>
-          <Button variant="ghost" size="sm" className="w-9 p-0">
-            <ChevronsUpDown className="h-4 w-4" />
-            <span className="sr-only">Toggle</span>
-          </Button>
-        </CollapsibleTrigger>
-      </div>
-      <div className="px-4">
-        {
-            subTitle && 
-            <Text 
-               style="text-sm text-[grey]"
-               value={subTitle}
-            />
-        }
-      </div>
-      <CollapsibleContent className="space-y-2">
-        <div className="px-4 h-full overflow-auto">
-            {children}
+        <div className="px-4">
+          {subTitle && <Text style="text-sm text-[grey]" value={subTitle} />}
         </div>
-      </CollapsibleContent>
-    </Collapsible>
+        <CollapsibleContent className="space-y-2">
+          <div className="px-4 h-full overflow-auto">{children}</div>
+        </CollapsibleContent>
+      </Collapsible>
     </div>
-  )
+  );
 }
